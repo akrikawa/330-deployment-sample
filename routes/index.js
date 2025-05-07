745c3d8289198865f684afa77f7a3883bf9c0347
Andy Krikawa <andy.krikawa@gmail.com>
2025-05-06 19:38:52 -0700

    Initial change to test deployment.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const router = Router();
 
-router.use("/items", require('./items'));
-router.get("/", (req, res, next) => {
+router.use('/items', require('./items'));
+router.get('/', (req, res, next) => {
   res.send(`
     <html>
       <body>
         <h1> Hello, world! </h1>
+        <p>This is a change and let's see if it deploys.</p>
       </body>
     </html>
-  `)
+  `);
 });
 
 module.exports = router;
