d5a2758cfcadce593372b1ed8e7cd61a63f81c8e
Andy Krikawa <andy.krikawa@gmail.com>
2025-05-13 19:53:14 -0700

    Added vite, cors, front-end folder.

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require("express");
-
-const routes = require("./routes");
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 server.use(routes);
 
 module.exports = server;
-
-
